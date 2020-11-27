307d0e240846eb35da75ede8b5fc4a3e9df0f06f
ItsWasted <roth.david00@gmail.com>
2020-11-27 12:12:57 +0100

    Finished crawler

---
+++ instagram-crawler.js
@@ -0,0 +1,8 @@
+var firstImg = $("._9AhH0")[0].click()
+
+setInterval(likeAndNext, 500)
+
+function likeAndNext() {
+  var likeIcon = $(".wpO6b")[1].click()
+  var nextButton = $(".coreSpriteRightPaginationArrow")[0].click()
+}
